258ec75eeca4eef649eaa7c58f00a132f10fd924
jaml <dev@jaml.pro>
2016-08-14 15:24:05 +0200

    Create and test the minTextLength directive

--- ng-text-length.js
+++ ng-text-length.js
@@ -3,4 +3,47 @@
 
   var app = angular.module('ngTextLength', []);
 
+  /**
+   * @ngdoc directive
+   * @name minTextLength
+   *
+   * @description
+   *
+   * More or equal than
+   */
+  app.directive('minTextLength', ['$sce', function($sce) {
+    return {
+      restrict: 'A',
+      require: '?ngModel',
+      link: function(scope, elem, attrs, ctrl) {
+        if (! ctrl) return;
+
+        var min;
+
+        attrs.$observe('minTextLength', function(value) {
+          min = parseInt(value, 10)
+          ctrl.$validate()
+        })
+
+        ctrl.$validators.minTextLength = function(_modelValue, viewValue) {
+
+          // Avoids problems with undefined and null values
+          if (ctrl.$isEmpty(viewValue))
+            return min == 0
+
+          // Strings that are not wrapped as HTML provoke that jqLite treats them as CSS selectors
+          var html;
+          try {
+            html = angular.element(viewValue)
+          } catch (e) {
+            html = angular.element('<hack>'+ viewValue +'</hack>')
+          }
+
+          // Only convert to text and check its length when the HTML length is enough
+          return viewValue.length >= min && html.text().length >= min
+        }
+      }
+    }
+  }])
+
 })(window.angular)
--- test/ng-text-length.spec.js
+++ test/ng-text-length.spec.js
@@ -5,9 +5,9 @@ describe('ng-text-length', ()=> {
     , $rootScope = null
     , $scope = null
 
-  beforeEach( ()=> {
-    app = angular.module('ngTextLength')
+  beforeEach(module('ngTextLength'))
 
+  beforeEach( ()=> {
     inject( (_$compile_, _$rootScope_)=> {
       $compile = _$compile_
       $rootScope = _$rootScope_
@@ -15,15 +15,73 @@ describe('ng-text-length', ()=> {
     })
   })
 
-  let compile = (html, scope)=> {
-    let element = $compile(html)(scope)
-    scope.$digest()
+  let compile = (html)=> {
+    let element = $compile(html)($scope)
+    $scope.$digest()
     return element
   }
 
+  let fieldTemplate = (min, max, value = '')=> {
+    let minLength = min === null ? '' : `min-text-length="${min}"`
+    let maxLength = max === null ? '' : `max-text-length="${max}"`
+    $scope.fieldValue = value
+
+    return `
+      <form name="testForm">
+        <input name='field' ${minLength} ${maxLength} ng-model="fieldValue" />
+      </form>` 
+  }
+
   describe('module', ()=> {
+    beforeEach( ()=> app = angular.module('ngTextLength') )
+
     it('is defined', ()=> expect(app).to.exist )
     it('does not have any dependency', ()=> expect(app.value('appName').requires).to.be.empty )
+  })
+
+  describe('minTextLength', ()=> {
+
+    it('treats "undefined" values as empty', ()=> {
+      compile(fieldTemplate(0, null, undefined))
+      expect($scope.testForm.field.$valid).to.be.true
+      compile(fieldTemplate(1, null, undefined))
+      expect($scope.testForm.field.$valid).to.be.false
+    })
+
+    it('treats "null" values as empty', ()=> {
+      compile(fieldTemplate(0, null, null))
+      expect($scope.testForm.field.$valid).to.be.true
+      compile(fieldTemplate(1, null, null))
+      expect($scope.testForm.field.$valid).to.be.false
+    })
+
+    it('is valid when the text of a textual ngModel is longer or equal than the minimum', ()=> {
+      compile(fieldTemplate(3, null))
+      expect($scope.testForm.field.$valid).to.be.false
+
+      compile(fieldTemplate(3, null, 'a'))
+      expect($scope.testForm.field.$valid).to.be.false
+
+      compile(fieldTemplate(3, null, 'aaa'))
+      expect($scope.testForm.field.$valid).to.be.true
+
+      compile(fieldTemplate(3, null, 'aaaa'))
+      expect($scope.testForm.field.$valid).to.be.true
+    })
+
+    it('is valid when the text of an HTML ngModel is longer or equal than the minimum', ()=> {
+      compile(fieldTemplate(3, null, '<a></a>'))
+      expect($scope.testForm.field.$valid).to.be.false
+
+      compile(fieldTemplate(3, null, '<a>a</a>'))
+      expect($scope.testForm.field.$valid).to.be.false
+
+      compile(fieldTemplate(3, null, '<a>aaa</a>'))
+      expect($scope.testForm.field.$valid).to.be.true
+
+      compile(fieldTemplate(3, null, '<a>aaaa</a>'))
+      expect($scope.testForm.field.$valid).to.be.true
+    })
   })
 
 })
